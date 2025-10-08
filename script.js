647695a4e86d8181e8abceafe8c50eb0864fa17c
Abid Hossain <hossain.abid.studente@itispaleocapa.it>
2025-10-08 20:24:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 // Evento 7 (inline) viene da HTML -> saluta()
-function saluta() {
+function saluta()
+  {
   const nome = document.getElementById("nome").value || "Visitatore";
   alert("Ciao, " + nome + "!");
 }
 
 // Eventi 8, 9, 10 (sul box)
-function boxOver() {
+function boxOver() 
+{
   const box = document.getElementById("box");
   box.style.backgroundColor = "lightgreen";
   box.innerText = "Mouse sopra!";
 }
 
-function boxOut() {
+function boxOut() 
+{
   const box = document.getElementById("box");
   box.style.backgroundColor = "lightgray";
   box.innerText = "Passa sopra o cliccami";
 }
 
-function boxClick() {
+function boxClick() 
+{
   const box = document.getElementById("box");
   box.style.backgroundColor = "salmon";
   box.innerText = "Hai cliccato!";
 }
 
-});
-});
+
 
 
 
