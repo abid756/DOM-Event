f61f3602d9bcb08d309bb544f6238bc07414d6ec
Abid Hossain <hossain.abid.studente@itispaleocapa.it>
2025-10-08 19:16:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,19 @@
-// JS esterno
-
-let contatore = 0;
-
-// Funzioni inline
-function incrementa() {
-  contatore++;
-  document.getElementById("contatore").innerText = contatore;
+function paginaCaricata() {
+  console.log("Pagina caricata correttamente!");
 }
 
-function azzera() {
-  contatore = 0;
-  document.getElementById("contatore").innerText = contatore;
+function ingrandisci() {
+  document.getElementById("immagine").style.width = "300px";
 }
 
-// Cambia colore di sfondo
-document.getElementById("rosso").addEventListener("click", function () {
-  document.body.style.backgroundColor = "lightcoral";
-});
+function riduci() {
+  document.getElementById("immagine").style.width = "150px";
+}
 
-document.getElementById("verde").addEventListener("click", function () {
-  document.body.style.backgroundColor = "lightgreen";
-});
+function coloraBox() {
+  document.getElementById("box").style.backgroundColor = "lightgreen";
+}
 
-document.getElementById("blu").addEventListener("click", function () {
-  document.body.style.backgroundColor = "lightblue";
-});
-
-// Mouseover su testo
-const testoHover = document.getElementById("hoverTesto");
-const testoOriginale = testoHover.innerText;
-
-testoHover.addEventListener("mouseover", function () {
-  this.innerText = "Grazie per aver passato il mouse!";
-});
-
-testoHover.addEventListener("mouseout", function () {
-  this.innerText = testoOriginale;
-});
-
-// Input dinamico
-document.getElementById("inputNome").addEventListener("input", function () {
-  let nome = this.value;
-  document.getElementById("saluto").innerText = "Ciao, " + nome + "!";
-});
-
-// Tema scuro
-document.getElementById("toggleTema").addEventListener("change", function () {
-  if (this.checked) {
-    document.body.classList.add("dark");
-  } else {
-    document.body.classList.remove("dark");
-  }
-});
-
-// Submit form
-document.getElementById("formFeedback").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("Grazie per il feedback!");
+document.getElementById("btnEsterno").addEventListener("click", function() {
+  alert("Evento gestito da file esterno!");
 });
